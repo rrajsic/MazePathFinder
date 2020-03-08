9e2a3b317057e213fa22e81dbafa6d21d2a5892c
Robin <61834606+rrajsic@users.noreply.github.com>
2020-03-08 17:09:01 +0100

    Add files via upload

---
+++ MazepathFinder/CharactersMark.h
@@ -0,0 +1,14 @@
+#ifndef CHARACTERSMARK_H
+#define CHARACTERSMARK_H
+struct Character
+{
+public:
+	static const unsigned char PLAYER = 167;
+	static const unsigned char WALLS = 219;
+	static const unsigned char OUTERWALLS = 178;
+	static const unsigned char PATH = ' ';
+	static const unsigned char EXIT = 'E';
+	
+};
+
+#endif
